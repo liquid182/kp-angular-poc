518348b79eb95c1e4b77d3dd89a9de3bc1a61979
Ryan McCullough <ryan.mccullough@perficient.com>
2017-09-27 12:21:06 -0700

    RM: Updating base project (minimal files).

--- ui.apps/src/main/content/jcr_root/etc/angular/base-project/src/app/polyfill.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("core-js/client/shim");
-require("zone.js/dist/zone");
-require("zone.js/dist/long-stack-trace-zone");
-require("../style.css");
